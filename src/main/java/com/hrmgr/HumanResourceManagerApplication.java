cd798ea716b60ff6c92cb663eb952d75f7e1373e
Harmanpreet6351 <harmanpreet181001@gmail.com>
2023-03-26 20:54:21 +0530

    Initial commit

---
+++ src/main/java/com/hrmgr/HumanResourceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hrmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrmgr/HumanResourceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
